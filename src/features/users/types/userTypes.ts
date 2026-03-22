2dc63ecf502317b8c1129006fb745a15589c026d
Marco Torres <marco.iturbide99@hotmail.es>
2026-03-22 17:48:46 -0600

    se agrega tiapdos para cada utilidad.

--- src/features/users/types/userTypes.ts
+++ src/features/users/types/userTypes.ts
@@ -1,19 +1,28 @@
-export interface LoginUser {
+export interface User {
   id: number
-  username: string
   email: string
   role: string
+  enabled: boolean
 }
 
 export interface CreateUserRequest {
-  username: string
   email: string
-  password: string
+  password?: string
   role: string
 }
 
-export interface UserUpdate {
-  username: string
+export interface UpdateUserRequest {
+  email?: string
+  password?: string
+  role?: string
+  enabled?: boolean
+}
+
+export interface ResponseUser {
+  id: number
   email: string
-  password: string
+  role: string
+  enabled: boolean
+  createdAt: string
+  updatedAt: string
 }
